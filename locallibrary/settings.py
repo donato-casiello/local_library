40563272953dee5e427d6ff0944e4bfec529f4cc
Donato Casiello <donato@MacBook-di-Donato.local>
2023-04-17 17:44:21 +0200

    Fix CSRF Trusted Origin

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -34,7 +34,7 @@ DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = ['web-production-d51b.up.railway.app', '127.0.0.1']
 
-CSRF_TRUSTED_ORIGINS =['web-production-d51b.up.railway.app']
+CSRF_TRUSTED_ORIGINS =['https://web-production-d51b.up.railway.app']
 
 
 # Application definition
